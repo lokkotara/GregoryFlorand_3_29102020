a9b2b945e5319a43b8f9774f34dd4b84b962ee55
Grégory <milton-the-wizard@hotmail.fr>
2020-11-06 09:03:47 +0100

    préparation environnement de travail

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const minify = require('gulp-clean-css');
+
+
+function scss2css() {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(minify())
+    .pipe(gulp.dest('./www/css'));
+}
+
+function autoUpdate(){
+  browserSync.init({
+    server: {
+      baseDir : "./www"
+    }
+  });
+  gulp.watch("./scss/*.scss", scss2css);
+  gulp.watch("./www/").on("change", browserSync.reload);
+}
+
+exports.scss2css = scss2css;
+exports.watch = autoUpdate;
